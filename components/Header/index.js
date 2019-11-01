8d37add315878f6c8ff8be562126732aeac3712f
Joseph Graham <Joseph@Chasingthefiks.net>
2019-11-01 09:20:35 -0600

    header.js MVP Met

--- components/Header/index.js
+++ components/Header/index.js
@@ -8,5 +8,28 @@
 //    <span class="temp">98°</span>
 //  </div >
 // And add it to the DOM in the .header-container component
+const hdrloc = document.querySelector('.header-container')
+hdrloc.appendChild(Header());
 
-function Header() {}
+function Header() {
+    //create elements
+    const hdr = document.createElement('div'), //header div
+        spdate = document.createElement('span'), //date span
+        httl = document.createElement('h1'), //title h1
+        sptemp = document.createElement('span'); //temp span
+    //name classes
+    hdr.classList.add('header');
+    spdate.classList.add('date');
+    sptemp.classList.add('temp');
+    //append element structure
+    hdr.appendChild(spdate);
+    hdr.appendChild(httl);
+    hdr.appendChild(sptemp);
+    //set content source
+    spdate.textContent = 'MARCH 28, 2019';
+    httl.textContent = 'Lambda Times';
+    sptemp.textContent = '98°';
+
+    //return build header div
+    return hdr
+}
