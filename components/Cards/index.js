4773637d29ea5f15cc068cd65a874d9d62131c85
Joseph Graham <Joseph@Chasingthefiks.net>
2019-11-01 10:39:38 -0600

    MVP Met all Elements

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,42 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+const cardEntry = document.querySelector(".cards-container"); //parent div
+//api call
+axios
+    .get("https://lambda-times-backend.herokuapp.com/articles") //get data
+    .then(response => {
+        const articles = Object.entries(response.data.articles); //create array from obj to loop
+        articles.forEach(item => { //loop array
+            item[1].forEach(i => { //loop building cards
+                const newArticle = Crdbldr(i);
+                cardEntry.appendChild(newArticle); //append card to parent div
+            });
+        });
+    });
+
+function Crdbldr(obj) { //card builder access obj data
+    const card = document.createElement("div"), //card container div
+        cardHeadline = document.createElement("div"), //card title div
+        cardAuthor = document.createElement("div"), //card author div
+        imgContainer = document.createElement("div"), //card image div
+        authorImg = document.createElement("img"), //card img
+        authorName = document.createElement("span"); //card name span
+    //set card names
+    card.classList.add("card");
+    cardHeadline.classList.add("headline");
+    cardAuthor.classList.add("author");
+    imgContainer.classList.add("img-container");
+    //set element structure        
+    card.appendChild(cardHeadline);
+    card.appendChild(cardAuthor);
+    cardAuthor.appendChild(imgContainer);
+    imgContainer.appendChild(authorImg);
+    cardAuthor.appendChild(authorName);
+    //set element content
+    cardHeadline.textContent = obj.headline;
+    authorName.textContent = obj.authorName;
+    authorImg.src = obj.authorPhoto;
+    //return built card
+    return card;
+};
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,31 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+//axios API call
+
+axios
+    .get("https://lambda-times-backend.herokuapp.com/topics") //get data
+    .then(response => {
+        console.log(response);
+        response.data.topics.forEach(element => { //loop data for seperate topics
+            const tpc = tbldr(element);
+            tbloc.appendChild(tpc); //append to parent location
+        });
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
+const tbloc = document.querySelector('.topics'); //parent div
+
+function tbldr(tbdta) { //tab builder accepts tab data object
+    //create elements
+    const tb = document.createElement('div');
+    //set tab name
+    tb.classList.add('tab');
+    //set element content
+    tb.textContent = tbdta;
+    //return topic div
+    return tb
+}
